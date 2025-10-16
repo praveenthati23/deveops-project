55e6dd91f5fdd7a318371aa1f5b2e44e385fa807
praveenthati23 <134906977+praveenthati23@users.noreply.github.com>
2025-10-16 14:56:36 +0530

    Update application.java

--- application.java
+++ application.java
@@ -2,5 +2,12 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("Hi! I am a developer.");
         System.out.println("I am new to this arena but going to do well here also!");
+
+        // Keep the app running
+        try {
+            Thread.sleep(Long.MAX_VALUE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
