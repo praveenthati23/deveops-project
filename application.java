1ab87ed7069ac5e273adb0a28ab36c1937d67124
praveenthati23 <134906977+praveenthati23@users.noreply.github.com>
2025-10-16 14:21:43 +0530

    Update application.java

--- application.java
+++ application.java
@@ -1,2 +1,6 @@
-hi am a developer
-am new to this arena but going to do well here also
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hi! I am a developer.");
+        System.out.println("I am new to this arena but going to do well here also!");
+    }
+}
